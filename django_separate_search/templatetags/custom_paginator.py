923b4c19dbddb60f1a79faa6cefc37004bff8f1a
Shailesh Kumar <shailesh.iiita+github@gmail.com>
2022-11-28 12:05:53 +0530

    mark current page for pagination

--- django_separate_search/templatetags/custom_paginator.py
+++ django_separate_search/templatetags/custom_paginator.py
@@ -7,6 +7,8 @@ register = template.Library()
 @register.inclusion_tag('admin/custom_pagination.html', takes_context=True)
 def custom_pagination(context, cl):
     pagination = admin_list.pagination(cl)
+
+    # TODO: Hacky way. Find a better way to do it.
     for cn in context:
         if "query_params" in cn:
             pagination['query_params'] = cn["query_params"]
